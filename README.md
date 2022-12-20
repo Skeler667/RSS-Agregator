19809bc891b33cf4c5bdcda0f50b9c08082f536a
SuperSergey <loloalilqwertykop@gmail.com>
2022-12-21 01:24:00 +0300

    1 step fix

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ yarn build
 ```
 
 to bundle your application
+### Actions 
+[![CI](https://github.com/Skeler667/frontend-project-11/actions/workflows/demo.yml/badge.svg?event=push)](https://github.com/Skeler667/frontend-project-11/actions/workflows/demo.yml)
+### Code Climate
+[![Maintainability](https://api.codeclimate.com/v1/badges/5df47e83973d08cba453/maintainability)](https://codeclimate.com/github/Skeler667/frontend-project-11/maintainability)
+### Hexlet tests and linter status:
+[![Actions Status](https://github.com/Skeler667/frontend-project-11/actions/workflows/hexlet-check/badge.svg)](https://github.com/Skeler667/frontend-project-11/actions)
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV == 'production';
 
 const stylesHandler = MiniCssExtractPlugin.loader;
 
 const config = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     open: true,
-    host: "localhost",
+    host: 'localhost',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: 'index.html',
     }),
 
     new MiniCssExtractPlugin(),
@@ -31,19 +31,19 @@ const config = {
     rules: [
       {
         test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
+        use: [stylesHandler, 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
+        use: [stylesHandler, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        type: 'asset',
       },
 
       // Add your rules for custom modules here
@@ -54,9 +54,9 @@ const config = {
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production';
   } else {
-    config.mode = "development";
+    config.mode = 'development';
   }
   return config;
 };
